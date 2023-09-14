6389db36c3134881ac01cc7a22751c99fe961d57
PeterSenyk <Peter.Senyk@gmail.com>
2023-09-14 07:49:13 -0700

    Attempt 1

---
+++ problem_2.py
@@ -0,0 +1,7 @@
+base_new = int(input("Give me a number between 2 & 9: "))
+print(base_new)
+decimal_max = base_new ** 4 - 1
+print(decimal_max)
+decimal_user = int(input("Please select a nmumber less than or equal to " + str(decimal_max)))
+print(decimal_user)
+
