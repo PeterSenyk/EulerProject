30fd7c3369a70b638406671f498101eaa96b3022
PeterSenyk <Peter.Senyk@gmail.com>
2023-09-15 08:13:45 -0700

    Added problem_2 (correct).

--- problem_2.py
+++ problem_2.py
@@ -1,18 +1,14 @@
 number_one = 1
 number_two = 2
-fib_number = 0
+fib_number = number_two
 sum = 0
 
 while fib_number <= 4000000:
     if fib_number % 2 == 0:
         sum += fib_number
 
-    fib_number = number_one
-    print("fib", fib_number)
-    number_two += number_one
-    print("two", number_two)
-    number_one += fib_number
-    print("one", number_one)
-
+    fib_number += number_one
+    number_one = number_two
+    number_two = fib_number
 
 print(sum)
