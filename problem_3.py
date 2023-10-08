6e0cf29574c16a9b7c3e4de2cb53a5a7d7a4b132
Peter Senyk <Peter.Senyk@gmail.com>
2023-10-07 19:17:40 -0700

    FIX problem_3

--- problem_3.py
+++ problem_3.py
@@ -1,22 +1,20 @@
 import math
-from typing import List, Any
 
 
 def is_factor(number):
     factors = []
-    i = 1
-    while i * i <= number:
-        if number % i == 0:
-            factors.append(int(i))
-            if number / i != i:
-                factors.append(int(number / i))
-        i += 1
+    for factor in range(2, math.ceil(math.sqrt(number))):
+        if number % factor == 0:
+            factors.append(int(factor))
+            if number / factor != factor:
+                factors.append(int(number / factor))
+        factor += 1
     factors.sort()
     return factors
 
 
 def is_number_prime(number):
-    for divisor in range(2, number):
+    for divisor in range(2, math.ceil(math.sqrt(number))):
         if number % divisor == 0:
             return False
     return True
@@ -26,11 +24,8 @@ initial_number = 600851475143
 prime_factor = []
 factor_list = is_factor(initial_number)
 
-
-
 for i in factor_list:
     if is_number_prime(i):
         prime_factor.append(i)
 
 print(max(prime_factor))
-
